be0e16ad960bab762caabdcf1e8d68e2338d8d57
Pirxcy <71592833+PirxcyFinal@users.noreply.github.com>
2023-01-22 21:00:29 +0000

    Update main_process_stop.py

--- example/cogs/main_process_stop.py
+++ example/cogs/main_process_stop.py
@@ -1,13 +1,16 @@
 import bettersanic as bs
 import asyncio
 
+class MainProcessStop(Exception):
+    pass
+
 class MainProcessStop(bs.Cog):
     def __init__(self, app: bs.BetterSanic) -> None:
         self.app = app
     
     @bs.listener("main_process_stop")
     async def main_process_stop(self, app: bs.BetterSanic, loop: asyncio.AbstractEventLoop):
-        print("The app has shut down.")
+        raise MainProcessStop("The App has been Terminated.")
 
 def setup(app: bs.BetterSanic):
     app.add_cog(MainProcessStop(app))
